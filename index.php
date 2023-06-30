19385c8bbc09e0d4292eba805a7d0a1b605ca12d
Ericson-Manalo <ericmana98@gmail.com>
2023-06-30 16:34:45 +0200

    added a title

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!-- Descrizione:
 > Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
 > Gestire il tutto con due file diversi. Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
-
-    stampare a schermo il paragrafo e la sua lunghezza
-    stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi tutte le occorrenze della parola da censurare-->
+stampare a schermo il paragrafo e la sua lunghezza
+stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi tutte le occorrenze della parola da censurare-->
 
 
 <!DOCTYPE html>
@@ -15,10 +14,17 @@
     <title>PHP BAD WORDS</title>
 </head>
 <body>
+    <h1>
+        PHP Bad Words
+    </h1>
     <form action="censored.php" method="GET">
         <textarea name="paragraph" cols="30" rows="10" placeholder="Insert a sentence"></textarea>
         <br>
         <input type="text" name="badWord" placeholder="Insert the bad word">
+
+        <button>
+            Censura
+        </button>
     </form>
 </body>
 </html>
