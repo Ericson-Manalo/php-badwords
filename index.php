d66cb58349d97a91c11473e28cf888c2c407546d
Ericson-Manalo <ericmana98@gmail.com>
2023-06-30 16:00:51 +0200

    created my form with a textarea for the paragraph and a input for the badword

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
+<!-- Descrizione:
+> Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
+> Gestire il tutto con due file diversi. Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+
+    stampare a schermo il paragrafo e la sua lunghezza
+    stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi tutte le occorrenze della parola da censurare-->
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP BAD WORDS</title>
 </head>
 <body>
-    <h1>
-        ciao come va
-    </h1>
+    <form action="censored.php" method="GET">
+        <textarea name="paragraph" cols="30" rows="10" placeholder="Insert a sentence"></textarea>
+        <br>
+        <input type="text" name="badWord" placeholder="Insert the bad word">
+    </form>
 </body>
 </html>
