3d5f1769f481060f9d8aa5cac4e78f6166a19d20
Ericson-Manalo <ericmana98@gmail.com>
2023-06-30 16:05:15 +0200

    created a variable to get the length of my paragraph

--- censored.php
+++ censored.php
@@ -1,6 +1,22 @@
 <?php
 
+$sentence = $_GET('paragraph');
+$badWord = $_GET('badWord');
 
+$textLength = strlen($sentence);
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
